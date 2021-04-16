6e1ddee850f7952bb287758cabd64cc2a3ba700b
pc <pc@qq.com>
2021-04-16 15:48:41 +0800

    完成主界面人物基础数据；

--- common/model/player.js
+++ common/model/player.js
@@ -8,7 +8,9 @@ function Player(pid) {
     this.clsName = "Player"
     this.db_table = 'player_info';
     this.db_idxField = 'pid';
-    this.db_fields = ["pid", 'uid', 'aid', "avatarid", 'name', 'lv', 'exp', 'vipLv', 'vipExp', "phone", "createTime", "loginTime", "logoutTime", "online"];
+    this.db_fields = ["pid", 'uid', 'aid', "avatarid", 'name', 'lv', 'exp',
+        'vipLv', 'vipExp', 'vitValue', 'vitMax', 'vigValue', 'vigMax',
+        "phone", "createTime", "loginTime", "logoutTime", "online"];
 }
 Player.create = async function (pid, idx) {
     let model = new Player(pid);
@@ -21,6 +23,11 @@ Player.prototype.inited = async function () {
 Player.prototype.loadDataed = async function () {
     this.currency.loadData();
 }
-
+// 获得下级所需经验
+Player.prototype.getNextLvExp = function (lv) {
+    lv = lv == null ? this.get_lv() : lv;
+    let exp = 30 * (Math.pow(lv, 3) + lv * 5) - 80
+    return exp
+}
 
 module.exports = Player
