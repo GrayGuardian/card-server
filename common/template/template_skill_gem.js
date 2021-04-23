2d5b96bc77a9925790f96b4f5523562f578a6b35
pc <pc@qq.com>
2021-04-23 13:00:12 +0800

    更新技能宝石表；更新协议；

--- common/template/template_skill_gem.js
+++ common/template/template_skill_gem.js
@@ -1,4 +1,8 @@
 const tpl=[
-{tid:1,name:'技能宝石一',pic_icon_type:1,},
+{tid:1,name:'技能宝石一',pic_icon_type:0,},
+{tid:2,name:'技能宝石二',pic_icon_type:1,},
+{tid:3,name:'技能宝石三',pic_icon_type:2,},
+{tid:4,name:'技能宝石四',pic_icon_type:3,},
+{tid:5,name:'技能宝石五',pic_icon_type:4,},
 ]
 module.exports = tpl
